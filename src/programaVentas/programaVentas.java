990f87e2fbfd537e0163616f8e5a5d2e6e048b6c
javierlago <javierlagoamoedor.daw@gmail.com>
2022-11-03 17:12:05 +0100

    añado ejercicio de ventas

--- src/programaVentas/programaVentas.java
+++ src/programaVentas/programaVentas.java
@@ -4,14 +4,20 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class programaVentas {
-    public static void main(String[] args ) throws IOException{
+    public static void main(String[] args ) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Introducir el nuemero de ventas");
         String numerodeventas = br.readLine();
         int nv = Integer.parseInt(numerodeventas);
-
-
-
-
+        int total = 0;
+        byte contadorventas =1;
+        for (int i = 0; i < nv; i++) {
+            System.out.println("Introduzca el valor de la venta nº "+contadorventas);
+            String sumadeventas = br.readLine();
+            int sv = Integer.parseInt(sumadeventas);
+            total = (total + sv);
+            contadorventas++;
+        }
+        System.out.println("El numero valor de las ventas realizadas es "+total);
     }
 }
